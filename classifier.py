enabled for GPUs, fixed softmax (?)

classifier.py
@@ -8,19 +8,20 @@ class MaskedClassifier(nn.Module):
         super().__init__()
         self.x_dim = x_dim
         self.mask = mask.to(device)
+        self.device = device
 
         self.all_classes_dim, = mask.shape
         self.net = nn.Sequential(nn.Linear(self.x_dim, self.all_classes_dim)).to(device)
 
     def forward(self, X):
         pred = self.net(X)
-        masked_pred = torch.where(self.mask, pred, torch.FloatTensor([-1e9]))
+        masked_pred = torch.where(self.mask, pred, torch.FloatTensor([-1e9]).to(self.device))
         return masked_pred
 
 
 def train_cls(cls_mask, train_X, train_y, val_X, val_y, device):
     _, fea_dim = train_X.shape
-    linear_cls = MaskedClassifier(fea_dim, cls_mask, device)
+    linear_cls = MaskedClassifier(fea_dim, cls_mask.to(device), device)
     optimizer = torch.optim.Adam(linear_cls.parameters())
     criterion = nn.CrossEntropyLoss()
 
@@ -29,7 +30,7 @@ def train_cls(cls_mask, train_X, train_y, val_X, val_y, device):
 
     best_val_acc = 0.0
     best_linear_cls = None
-    for i in range(5): # 100
+    for i in range(10): # 100
         optimizer.zero_grad()
         train_pred = linear_cls(train_X.to(device))
         loss = criterion(train_pred, train_y)

dataset.py
@@ -94,7 +94,7 @@ class ParentDataset:
 
         assert self.attributes.shape[0] == len(self.class_name_to_idx)
 
-    def get_dataset(self, type):
+    def get_dataset(self, type, device):
         """
         :param type: name of the data set to get: 'train', 'val', 'trainval', 'test_seen', 'test_unseen'.
         :returns: (features, labels, mask) tuple, where:
@@ -102,12 +102,12 @@ class ParentDataset:
             * labels - labels of shape (N,), where each element is a number from 0 to C-1.
             * mask - mask vector with 1 where legal classes of this dataset are, of shape (C,)
         """
-        features = torch.FloatTensor(self.features[self._attribute_dict[f'{type}_loc'].reshape(-1) - 1, :])
-        labels = torch.LongTensor(self.labels[self._attribute_dict[f'{type}_loc'].reshape(-1) - 1])
+        features = torch.FloatTensor(self.features[self._attribute_dict[f'{type}_loc'].reshape(-1) - 1, :]).to(device)
+        labels = torch.LongTensor(self.labels[self._attribute_dict[f'{type}_loc'].reshape(-1) - 1]).to(device)
         classes = torch.unique(labels)
-        mask = torch.zeros((self.class_num,), dtype=torch.bool)
+        mask = torch.zeros((self.class_num,), dtype=torch.bool).to(device)
         for label in classes:
-            mask[label.item()] = 1
+            mask[label.item()] = True
         return features, labels, classes, mask
     
 

gan.py
@@ -132,7 +132,7 @@ class GanTrainer:
 
         if use_cls_loss:
             self.classifier.eval()
-            Y_pred = F.softmax(self.classifier(X_gen), dim=0)
+            Y_pred = F.softmax(self.classifier(X_gen), dim=-1)
             log_prob = torch.log(torch.gather(Y_pred, 1, label_idx.unsqueeze(1)))
             L_cls = -1 * torch.mean(log_prob)
             L_gen += self.beta * L_cls
@@ -164,15 +164,15 @@ class GanTrainer:
         '''
         self.net_G.eval()
 
-        y = np.tile(classes, n_examples)
-        attr = torch.FloatTensor(attributes[y], device=self.device)
-        z = self.Z_dist.sample(torch.Size((len(classes) * n_examples, self.z_dim)))
+        y = torch.tile(classes, (n_examples,)).to(self.device)
+        attr = torch.index_select(torch.FloatTensor(attributes).to(self.device), 0, y)
+        z = self.Z_dist.sample(torch.Size((len(classes) * n_examples, self.z_dim))).to(self.device)
         z_inp = self.get_conditional_input(z, attr)
         X_gen = self.net_G(z_inp).detach()
 
         assert X_gen.shape[0] == len(classes) * n_examples
 
-        return X_gen, torch.LongTensor(y, device=self.device)
+        return X_gen, y
 
     def save_model(self):
         g_ckpt_path = os.path.join(self.model_save_dir, "netG.pth")

main.py
@@ -10,8 +10,8 @@ from gan import GanTrainer
 from classifier import MaskedClassifier, train_cls
 
 
-def get_dataset_and_print(data, type):
-    X, y, classes, mask = data.get_dataset(type)
+def get_dataset_and_print(data, type, device):
+    X, y, classes, mask = data.get_dataset(type, device)
     print('---------------')
     print(f'Data set {type}:')
     print(f'  N:          {X.shape[0]}')
@@ -21,14 +21,14 @@ def get_dataset_and_print(data, type):
     return X, y, classes, mask
 
 def cli():
-    device = torch.device('cpu') #torch.device('cuda') if torch.cuda.is_available() else torch.device('cpu')
+    device = torch.device('cuda') if torch.cuda.is_available() else torch.device('cpu')
     print(f'Device: {device}')
 
     data_features_dir = '/mnt/c/datasets/xlsa17/data/AWA2'
     data = dataset.ParentDataset(data_features_dir)
 
     total_N, _ = data.features.shape
-    train_all_X, train_all_y, train_all_classes, train_all_mask = get_dataset_and_print(data, 'train')
+    train_all_X, train_all_y, train_all_classes, train_all_mask = get_dataset_and_print(data, 'train', device)
     train_all_N, fea_dim = train_all_X.shape
 
     # TODO: make gin configurable?
@@ -38,9 +38,9 @@ def cli():
     train_X, train_y, train_mask = train_all_X[:train_N, :], train_all_y[:train_N], train_all_mask
     val_seen_X, val_seen_y, val_seen_mask = train_all_X[train_N:, :], train_all_y[train_N:], train_all_mask
 
-    val_unseen_X, val_unseen_y, val_unseen_classes, val_unseen_mask = get_dataset_and_print(data, 'val')
-    test_seen_X, test_seen_y, test_seen_classes, test_seen_mask = get_dataset_and_print(data, 'test_seen')
-    test_unseen_X, test_unseen_y, test_unseen_classes, test_unseen_mask = get_dataset_and_print(data, 'test_unseen')
+    val_unseen_X, val_unseen_y, val_unseen_classes, val_unseen_mask = get_dataset_and_print(data, 'val', device)
+    test_seen_X, test_seen_y, test_seen_classes, test_seen_mask = get_dataset_and_print(data, 'test_seen', device)
+    test_unseen_X, test_unseen_y, test_unseen_classes, test_unseen_mask = get_dataset_and_print(data, 'test_unseen', device)
 
     assert total_N == train_all_X.shape[0] + val_unseen_X.shape[0] + test_seen_X.shape[0] + test_unseen_X.shape[0]
 
@@ -52,11 +52,11 @@ def cli():
     print('2. Train  the  conditional f-CLSWGAN generator, conditioned on class attributes a_y')
 
     attr_dim = data.attributes.shape[-1]
-    train_attr = torch.FloatTensor(data.attributes[train_y.detach().numpy()])
+    train_attr = torch.FloatTensor(data.attributes[train_y.cpu().detach().numpy()])
     assert train_attr.shape == (train_X.shape[0], attr_dim)
     gan_trainer = GanTrainer(device, fea_dim, attr_dim, attr_dim, classifier=best_linear_cls)
 
-    n_epochs = 5
+    n_epochs = 10
     for ep in trange(1, n_epochs + 1):
         loss_dis, loss_gen = gan_trainer.fit_GAN(train_X, train_attr, train_y)
         print("Loss for epoch: %3d - D: %.4f | G: %.4f"\